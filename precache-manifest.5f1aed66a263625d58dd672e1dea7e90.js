dca893f71a379c10404f0623cc01dc1eec12db40
Jakub Najman <jakub@najman.cz>
2024-04-03 15:09:57 +0200

    Updates

---
+++ precache-manifest.5f1aed66a263625d58dd672e1dea7e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9134c9b602344771d6d8319bc946704",
+    "url": "/JakkNaj/react-hangman-kaj/index.html"
+  },
+  {
+    "revision": "772866bb0d81152b8ffb",
+    "url": "/JakkNaj/react-hangman-kaj/static/css/main.bfd10ace.chunk.css"
+  },
+  {
+    "revision": "5c269f28dcb94ac73b93",
+    "url": "/JakkNaj/react-hangman-kaj/static/js/2.91802fdb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/JakkNaj/react-hangman-kaj/static/js/2.91802fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772866bb0d81152b8ffb",
+    "url": "/JakkNaj/react-hangman-kaj/static/js/main.693eac7b.chunk.js"
+  },
+  {
+    "revision": "a9569f664c6856998a6a",
+    "url": "/JakkNaj/react-hangman-kaj/static/js/runtime-main.0f4f323c.js"
+  },
+  {
+    "revision": "00670d0d5afb1c55be8bbbd3e36dc0bc",
+    "url": "/JakkNaj/react-hangman-kaj/static/media/lose-sound.00670d0d.mp3"
+  },
+  {
+    "revision": "5ea1467bf2e08ec38da218017dcded2d",
+    "url": "/JakkNaj/react-hangman-kaj/static/media/win-sound.5ea1467b.mp3"
+  }
+]);
