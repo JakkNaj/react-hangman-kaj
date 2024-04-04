a0cbe81419147a38d364d664e65ef494f7f1cb35
Jakub Najman <jakub@najman.cz>
2024-04-04 20:46:44 +0200

    Top Players page

--- src/components/TopPlayers.js
+++ src/components/TopPlayers.js
@@ -1,38 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import {getAllPlayers} from "../modules/localStorageManipulator";
-import {useGameStatus} from "./GameStatusContext";
-
 import './TopPlayers.css';
 
 const TopPlayers = () => {
-    const [players, setPlayers] = useState([]);
+    const [topPlayers, setTopPlayers] = useState([]);
+    const [otherPlayers, setOtherPlayers] = useState([]);
 
     useEffect(() => {
         const fetchPlayers = async () => {
             const fetchedPlayers = getAllPlayers();
-            setPlayers(
-                fetchedPlayers
-                    .sort((a, b) => b.score - a.score)
-                    .slice(0,3)
-            );
+            const sortedPlayers = fetchedPlayers.sort((a, b) => b.score - a.score);
+            setTopPlayers(sortedPlayers.slice(0,3));
+            setOtherPlayers(sortedPlayers.slice(3));
         };
 
         fetchPlayers();
     }, []);
 
-
     return (
         <div className="top-players-section">
-            <h2>Top Players</h2>
             <ol className="top-players-list">
-                {players.map((player, index) => (
+                {topPlayers.map((player, index) => (
                     <li key={index}>
                         {player.name}
-                        <br />
+                        <br/>
                         {player.score}
                     </li>
                 ))}
             </ol>
+            <section className="heading">
+                <h2>Player's name:</h2>
+                <h2>Score:</h2>
+            </section>
+            <ul className="other-players-list">
+                {otherPlayers.map((player, index) => (
+                    <li key={index}>
+                        <section className="others">
+                            <h2>{player.name}</h2>
+                            <h2>{player.score}</h2>
+                        </section>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
